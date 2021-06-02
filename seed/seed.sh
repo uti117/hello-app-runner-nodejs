1bd94c3f76ac383478e3a875fac756f57646a528
Yuta Ishii <ishiiyut@amazon.co.jp>
2021-06-02 06:32:09 +0000

    My first push-to-deploy with CodePipeline

--- seed/seed.sh
+++ seed/seed.sh
@@ -11,7 +11,7 @@ echo "DynamoDB table name: ${DDB_TABLE_NAME}"
 DATA=$(sed "s/TABLE_NAME/${DDB_TABLE_NAME}/g" < ./seed/data.json)
 
 echo "Seeding initial data to the DynamoDB table ..."
-RESULT=$(aws dynamodb batch-write-item --request-items "${DATA}")
+RESULT=$(/usr/local/bin/aws dynamodb batch-write-item --request-items "${DATA}")
 UNPROCESSED_ITEM=$(echo "${RESULT}" | jq -r '. | select(.UnprocessedItems | length > 0)')
 if [[ -z "${UNPROCESSED_ITEM}" ]]; then
   echo "Done!"
